4b08cedddbbabae54761f2f35ece283e3b0d4c81
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2024-08-29 23:08:55 +0200

    feat: add swagger and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,18 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { LookupsModule } from './lookups/lookups.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
       port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'nest-news-app_db',
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity.{js,ts}'],
       synchronize: true,
     }),
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // config swager
+  const config = new DocumentBuilder()
+    .setTitle('News App')
+    .setDescription('The news app API description')
+    .setVersion('1.0')
+    .addTag('news-app')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
